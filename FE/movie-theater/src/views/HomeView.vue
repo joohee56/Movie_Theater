6e567dbe389d1563cd27a4758cc1dc10c4a4cbf7
Joohui <joohuii96@gmail.com>
2024-10-29 01:08:22 +0900

    [FE] feat: 프로젝트 생성

---
+++ FE/movie-theater/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template lang="ko">
+	<div>
+		Home View
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ FE/movie-theater/src/views/MovieView.vue
@@ -0,0 +1,9 @@
+<template lang="ko">
+  <div>movieView</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
