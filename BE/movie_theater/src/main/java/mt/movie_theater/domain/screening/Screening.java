7e71961aa11c4751594c547f5f376dd022c2c7be
Joohui <joohuii96@gmail.com>
2024-10-31 02:58:03 +0900

    [BE] feat: 상영 시간 도메인 생성

--- BE/movie_theater/src/main/java/mt/movie_theater/domain/screening/Screening.java
+++ BE/movie_theater/src/main/java/mt/movie_theater/domain/screening/Screening.java
@@ -1,2 +1,31 @@
-package mt.movie_theater.domain.screening;public class Screening {
+package mt.movie_theater.domain.screening;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import mt.movie_theater.domain.hall.Hall;
+import mt.movie_theater.domain.movie.Movie;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Screening {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Hall hall;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Long price;
 }
