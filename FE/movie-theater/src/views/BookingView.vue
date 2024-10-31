20edd5f49e48f36d8e1d01fe3f4e87b5fad9b12a
Joohui <joohuii96@gmail.com>
2024-11-01 02:33:53 +0900

    [FE] feat: vue2-datepicker 적용

--- FE/movie-theater/src/views/BookingView.vue
+++ FE/movie-theater/src/views/BookingView.vue
@@ -1,15 +1,31 @@
 <template lang="ko">
 	<div>
 		<PageTitle pageTitle="빠른예매"></PageTitle>
+		<date-picker v-model="selectedDate" format="YYYY.MM.DD" :lang="lang" />
 	</div>
 </template>
 
 <script>
+import DatePicker from "vue2-datepicker";
+import "vue2-datepicker/index.css";
+import "vue2-datepicker/locale/ko";
 import PageTitle from "@/components/header/PageTitle";
 
 export default {
+  data() {
+    return {
+      selectedDate: null,
+      lang: {
+        formatLocale: {
+          firstDayOfWeek: 1,
+        },
+        monthBeforeYear: false,
+      },
+    };
+  },
   components: {
     PageTitle,
+    DatePicker,
   },
 };
 </script>
