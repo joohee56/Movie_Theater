b57e3927b200d24b1fd9b41ae00dac536e5d9929
Joohui <joohuii96@gmail.com>
2024-11-01 13:48:40 +0900

    [BE] feat, test: 지역별 영화관 갯수 조회 컨트롤러, http 테스트

--- BE/movie_theater/src/main/java/mt/movie_theater/api/controller/theater/TheaterController.java
+++ BE/movie_theater/src/main/java/mt/movie_theater/api/controller/theater/TheaterController.java
@@ -1,11 +1,14 @@
 package mt.movie_theater.api.controller.theater;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mt.movie_theater.api.apiResponse.ApiResponse;
 import mt.movie_theater.api.controller.theater.request.TheaterCreateRequest;
+import mt.movie_theater.api.controller.theater.response.TheaterCountResponse;
 import mt.movie_theater.api.controller.theater.response.TheaterResponse;
 import mt.movie_theater.api.service.theater.TheaterService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,11 @@ public class TheaterController {
     public ApiResponse<TheaterResponse> createTheater(@Valid @RequestBody TheaterCreateRequest request) {
         TheaterResponse response = theaterService.createTheater(request);
         return ApiResponse.ok(response);
+    }
+
+    @GetMapping("/count")
+    public ApiResponse<List<TheaterCountResponse>> getRegionListAndTheaterCount() {
+        List<TheaterCountResponse> responses = theaterService.getRegionListAndTheaterCount();
+        return ApiResponse.ok(responses);
     }
 }
