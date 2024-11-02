d1e4347279e658883e5f60660e32286dfbcce11a
Joohui <joohuii96@gmail.com>
2024-11-03 02:16:06 +0900

    [BE] style: 사용하지 않는 코드 제거

--- BE/movie_theater/src/main/java/mt/movie_theater/domain/screening/ScreeningRepository.java
+++ BE/movie_theater/src/main/java/mt/movie_theater/domain/screening/ScreeningRepository.java
@@ -10,17 +10,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScreeningRepository extends JpaRepository<Screening, Long> {
-
-//    @Query("select s from Screening s where s.startTime >= :startDateTime and s.startTime < :endDateTime " +
-//            "and (:movieId is null or s.movie.id = :movieId) " +
-//            "and (:theaterId is null or s.hall.theater.id = :theaterId) order by s.startTime asc ")
-//    List<Screening> findByDateAndMovieIdAndTheaterId(
-//            @Param("startOfDay") LocalDateTime startDateTime,
-//            @Param("endOfDay") LocalDateTime endDateTime,
-//            @Param("movieId") Long movieId,
-//            @Param("theaterId") Long theaterId
-//    );
-
     @Query("select new mt.movie_theater.domain.screening.dto.RegionTheaterCountDto(t.region, count(s)) from Screening s join s.hall.theater t " +
             "where s.startTime >= :startDateTime and s.startTime < :endDateTime "
             + "and (:movieId is null or s.movie.id = :movieId) "
