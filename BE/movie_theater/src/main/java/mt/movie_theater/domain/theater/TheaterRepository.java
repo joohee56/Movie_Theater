b5a58da1868f1ff3c27452dfc8bd371ad90b823d
Joohui <joohuii96@gmail.com>
2024-11-03 15:36:43 +0900

    [BE] feat, test: 지역에 속하는 영화관리스트 조회 및 테스트

--- BE/movie_theater/src/main/java/mt/movie_theater/domain/theater/TheaterRepository.java
+++ BE/movie_theater/src/main/java/mt/movie_theater/domain/theater/TheaterRepository.java
@@ -1,8 +1,11 @@
 package mt.movie_theater.domain.theater;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TheaterRepository extends JpaRepository<Theater, Long> {
+    List<Theater> findALlByRegion(@Param("region") Region region);
 }
--- BE/movie_theater/src/test/java/mt/movie_theater/domain/theater/TheaterRepositoryTest.java
+++ BE/movie_theater/src/test/java/mt/movie_theater/domain/theater/TheaterRepositoryTest.java
@@ -3,12 +3,9 @@ package mt.movie_theater.domain.theater;
 import static mt.movie_theater.domain.theater.Region.GYEONGGI;
 import static mt.movie_theater.domain.theater.Region.SEOUL;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
 
 import java.util.List;
 import mt.movie_theater.IntegrationTestSupport;
-import mt.movie_theater.domain.theater.dto.RegionTheaterCountDto;
-import mt.movie_theater.domain.theater.dto.TheaterIdNameDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,4 +13,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 class TheaterRepositoryTest extends IntegrationTestSupport {
+    @Autowired
+    private TheaterRepository theaterRepository;
+
+    @DisplayName("지역에 속하는 영화관 리스트를 조회한다.")
+    @Test
+    void findALlByRegion() {
+        //given
+        createTheater(SEOUL, "강남");
+        createTheater(SEOUL, "강동");
+        createTheater(SEOUL, "군자");
+        createTheater(GYEONGGI, "고양스타필드");
+        createTheater(GYEONGGI, "광명ak플라자");
+
+        Region region = SEOUL;
+
+        //when
+        List<Theater> theaters = theaterRepository.findALlByRegion(region);
+
+        //then
+        assertThat(theaters).hasSize(3)
+                .extracting("name")
+                .containsExactlyInAnyOrder("강남", "강동", "군자");
+    }
+
+    private Theater createTheater(Region region, String name) {
+        Theater theater = Theater.builder()
+                .region(region)
+                .name(name)
+                .build();
+        return theaterRepository.save(theater);
+    }
 }
