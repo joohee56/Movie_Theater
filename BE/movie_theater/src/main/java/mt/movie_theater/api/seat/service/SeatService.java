ae7829133bc27d18a0097223b35127f702f34919
Joohui <joohuii96@gmail.com>
2024-11-05 02:44:51 +0900

    [BE] feat, test: 상영관에 해당하는 좌석리스트 조회 service 및 테스트

--- BE/movie_theater/src/main/java/mt/movie_theater/api/seat/service/SeatService.java
+++ BE/movie_theater/src/main/java/mt/movie_theater/api/seat/service/SeatService.java
@@ -2,11 +2,13 @@ package mt.movie_theater.api.seat.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import mt.movie_theater.api.seat.request.SeatListCreateRequest;
 import mt.movie_theater.api.seat.response.SeatResponse;
+import mt.movie_theater.api.seat.response.SeatSummaryResponse;
 import mt.movie_theater.domain.hall.Hall;
 import mt.movie_theater.domain.hall.HallRepository;
 import mt.movie_theater.domain.seat.Seat;
@@ -49,5 +51,18 @@ public class SeatService {
         return savedSeats.stream()
                 .map(seat -> SeatResponse.create(seat))
                 .collect(Collectors.toList());
+    }
+
+    public Map<String, List<SeatSummaryResponse>> getSeatList(Long hallId) {
+        List<Seat> seats = seatRepository.findAllByHall(hallId);
+        Map<String, List<Seat>> sectionSeatMap = seats.stream()
+                .collect(Collectors.groupingBy(Seat::getSection));
+        return sectionSeatMap.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().stream()
+                                .map(seat -> SeatSummaryResponse.create(seat))
+                                .collect(Collectors.toList())
+                ));
     }
 }
--- BE/movie_theater/src/test/java/mt/movie_theater/api/seat/service/SeatServiceTest.java
+++ BE/movie_theater/src/test/java/mt/movie_theater/api/seat/service/SeatServiceTest.java
@@ -5,15 +5,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.tuple;
 
 import java.util.List;
+import java.util.Map;
 import mt.movie_theater.IntegrationTestSupport;
 import mt.movie_theater.api.seat.request.SeatListCreateRequest;
 import mt.movie_theater.api.seat.response.SeatResponse;
+import mt.movie_theater.api.seat.response.SeatSummaryResponse;
 import mt.movie_theater.domain.hall.Hall;
 import mt.movie_theater.domain.hall.HallRepository;
-import mt.movie_theater.domain.movie.ScreeningType;
+import mt.movie_theater.domain.seat.Seat;
 import mt.movie_theater.domain.seat.SeatRepository;
-import mt.movie_theater.domain.theater.Region;
-import mt.movie_theater.domain.theater.Theater;
 import mt.movie_theater.domain.theater.TheaterRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,21 +43,9 @@ class SeatServiceTest extends IntegrationTestSupport {
     @Test
     void createSeatList() {
         //given
-        Theater theater = Theater.builder()
-                .name("창동")
-                .region(Region.SEOUL)
-                .build();
-        Theater savedTheater = theaterRepository.save(theater);
-
-        Hall hall = Hall.builder()
-                .theater(savedTheater)
-                .screeningType(ScreeningType.TWO_D)
-                .name("1관")
-                .build();
-        Hall savedHall = hallRepository.save(hall);
-
+        Hall hall = createHall();
         SeatListCreateRequest request = SeatListCreateRequest.builder()
-                .hallId(savedHall.getId())
+                .hallId(hall.getId())
                 .rows(2)
                 .columns(3)
                 .build();
@@ -92,5 +80,51 @@ class SeatServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(() -> seatService.createSeatList(request))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("유효하지 않은 상영관입니다. 상영관 정보를 다시 확인해 주세요.");
+    }
+
+    @DisplayName("상영관의 좌석 리스트를 Map 형태로 조죄한다.")
+    @Test
+    void getSeatList() {
+        //given
+        Hall hall1 = createHall();
+        Hall hall2 = createHall();
+        createSeat(hall1, "A", "1");
+        createSeat(hall1, "A", "2");
+        createSeat(hall1, "B", "1");
+        createSeat(hall1, "B", "2");
+        createSeat(hall2, "A", "1");
+
+        //when
+//        Map<String, List<SeatSummaryResponse>> sectionSeatMap = seatService.getSeatList(hall1.getId());
+        Map<String, List<SeatSummaryResponse>> sectionSeatMap = seatService.getSeatList(hall1.getId());
+
+        //then
+        assertThat(sectionSeatMap).hasSize(2);
+        assertThat(sectionSeatMap.get("A")).hasSize(2)
+                .extracting("section", "seatNumber")
+                .containsExactlyInAnyOrder(
+                        tuple("A", "1"),
+                        tuple("A", "2")
+                );
+        assertThat(sectionSeatMap.get("B")).hasSize(2)
+                .extracting("section", "seatNumber")
+                .containsExactlyInAnyOrder(
+                        tuple("B", "1"),
+                        tuple("B", "2")
+                );
+    }
+
+    private Hall createHall() {
+        Hall hall = Hall.builder()
+                .build();
+        return hallRepository.save(hall);
+    }
+    private Seat createSeat(Hall hall, String section, String seatNumber) {
+        Seat seat = Seat.builder()
+                .hall(hall)
+                .section(section)
+                .seatNumber(seatNumber)
+                .build();
+        return seatRepository.save(seat);
     }
 }
