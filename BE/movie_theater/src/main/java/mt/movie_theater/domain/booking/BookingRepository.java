aadbb2c942650c87e5f32a500cad9f155ec19fb1
Joohui <joohuii96@gmail.com>
2024-11-06 14:43:17 +0900

    [BE] feat, test: 회원에 해당하는 전체 예매 내역 조회 및 테스트

--- BE/movie_theater/src/main/java/mt/movie_theater/domain/booking/BookingRepository.java
+++ BE/movie_theater/src/main/java/mt/movie_theater/domain/booking/BookingRepository.java
@@ -1,12 +1,18 @@
 package mt.movie_theater.domain.booking;
 
+import java.util.List;
 import java.util.Optional;
 import mt.movie_theater.domain.screening.Screening;
 import mt.movie_theater.domain.seat.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     Optional<Booking> findByScreeningAndSeat(Screening screening, Seat seat);
+
+    @Query("select b from Booking b where b.user.id= :userId order by b.updatedAt desc")
+    List<Booking> findAllByUserId(@Param("userId") Long userId);
 }
--- BE/movie_theater/src/test/java/mt/movie_theater/domain/booking/BookingRepositoryTest.java
+++ BE/movie_theater/src/test/java/mt/movie_theater/domain/booking/BookingRepositoryTest.java
@@ -1,14 +1,18 @@
 package mt.movie_theater.domain.booking;
 
+import static mt.movie_theater.domain.booking.BookingStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
 
+import java.util.List;
 import java.util.Optional;
 import mt.movie_theater.IntegrationTestSupport;
 import mt.movie_theater.domain.screening.Screening;
 import mt.movie_theater.domain.screening.ScreeningRepository;
 import mt.movie_theater.domain.seat.Seat;
-import mt.movie_theater.domain.seat.SeatLocation;
 import mt.movie_theater.domain.seat.SeatRepository;
+import mt.movie_theater.domain.user.User;
+import mt.movie_theater.domain.user.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,25 +26,19 @@ class BookingRepositoryTest extends IntegrationTestSupport {
     private ScreeningRepository screeningRepository;
     @Autowired
     private SeatRepository seatRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @DisplayName("상영시간과 좌석에 해당하는 예매를 조회한다.")
     @Test
     void findByScreeningIdAndSeatId() {
         //given
-        Screening screening = Screening.builder()
-                .build();
-        Screening savedScreening = screeningRepository.save(screening);
-        Seat seat = Seat.builder()
-                .build();
-        Seat savedSeat = seatRepository.save(seat);
-        Booking booking = Booking.builder()
-                .screening(screening)
-                .seat(seat)
-                .build();
-        bookingRepository.save(booking);
+        Screening screening = createScreening();
+        Seat seat = createSeat();
+        createBooking(screening, seat);
 
         //when
-        Optional<Booking> findBooking = bookingRepository.findByScreeningAndSeat(savedScreening, savedSeat);
+        Optional<Booking> findBooking = bookingRepository.findByScreeningAndSeat(screening, seat);
         //then
         assertThat(findBooking).isPresent();
     }
@@ -49,28 +47,72 @@ class BookingRepositoryTest extends IntegrationTestSupport {
     @Test
     void findByScreeningIdAndSeatIdWithEmpty() {
         //given
-        Screening screening = Screening.builder()
-                                .build();
-        Screening savedScreening = screeningRepository.save(screening);
-
-        Seat seat1 = Seat.builder()
-                        .seatLocation(new SeatLocation("A", "1"))
-                        .build();
-        Seat seat2 = Seat.builder()
-                        .seatLocation(new SeatLocation("B", "1"))
-                        .build();
-        Seat savedSeat1 = seatRepository.save(seat1);
-        Seat savedSeat2 = seatRepository.save(seat2);
-        Booking booking = Booking.builder()
-                .screening(screening)
-                .seat(savedSeat1)
-                .build();
+        Screening screening = createScreening();
+        Seat seat1 = createSeat();
+        Seat seat2 = createSeat();
+        Booking booking = createBooking(screening, seat1);
         bookingRepository.save(booking);
 
         //when
-        Optional<Booking> findBooking = bookingRepository.findByScreeningAndSeat(savedScreening, savedSeat2);
+        Optional<Booking> findBooking = bookingRepository.findByScreeningAndSeat(screening, seat2);
         //then
         assertThat(findBooking).isEmpty();
+    }
+
+    @DisplayName("회원에 해당하는 전체 예매 내역을 수정일자 내림차순으로 조회한다.")
+    @Test
+    void findAllByUserId() {
+        //given
+        User user1 = createUser();
+        User user2 = createUser();
+
+        Booking booking1 = createBooking(user1, CONFIRMED);
+        Booking booking2 = createBooking(user1, CONFIRMED);
+        Booking booking3 = createBooking(user1, CONFIRMED);
+        booking2.cancel();
+
+        Booking booking4 = createBooking(user2, CONFIRMED);
+
+        //when
+        List<Booking> bookings = bookingRepository.findAllByUserId(user1.getId());
+
+        //then
+        assertThat(bookings).hasSize(3)
+                .extracting("user", "id", "bookingStatus")
+                .containsExactly(
+                  tuple(user1, booking2.getId(), CANCELED),
+                  tuple(user1, booking3.getId(), CONFIRMED),
+                  tuple(user1, booking1.getId(), CONFIRMED)
+                );
+    }
+
+    private User createUser() {
+        User user = User.builder().build();
+        return userRepository.save(user);
+    }
+    private Screening createScreening() {
+        Screening screening = Screening.builder().build();
+        return screeningRepository.save(screening);
+    }
+    private Seat createSeat() {
+        Seat seat = Seat.builder().build();
+        return seatRepository.save(seat);
+    }
+    private Booking createBooking(Screening screening, Seat seat) {
+        Booking booking = Booking.builder()
+                .screening(screening)
+                .seat(seat)
+                .build();
+        return bookingRepository.save(booking);
+    }
+    private Booking createBooking(User user, BookingStatus bookingStatus) {
+        Booking booking = Booking.builder()
+                .user(user)
+                .screening(createScreening())
+                .seat(createSeat())
+                .bookingStatus(bookingStatus)
+                .build();
+        return bookingRepository.save(booking);
     }
 
 }
