dc99d4c5d76218a55ad3735326cda6eb0a47860a
Joohui <joohuii96@gmail.com>
2024-11-10 16:49:32 +0900

    [BE] feat: 로그아웃 구현

--- BE/movie_theater/src/main/java/mt/movie_theater/api/user/controller/UserController.java
+++ BE/movie_theater/src/main/java/mt/movie_theater/api/user/controller/UserController.java
@@ -4,12 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mt.movie_theater.api.apiResponse.ApiResponse;
+import mt.movie_theater.api.user.annotation.LoginCheck;
 import mt.movie_theater.api.user.request.UserCreateRequest;
 import mt.movie_theater.api.user.request.UserLoginRequest;
 import mt.movie_theater.api.user.response.UserResponse;
 import mt.movie_theater.api.user.service.SessionService;
 import mt.movie_theater.api.user.service.UserService;
 import mt.movie_theater.domain.user.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,12 @@ public class UserController {
         User user = userService.authenticate(userLoginRequest.getLoginId(), userLoginRequest.getPassword());
         sessionService.createLoginSession(request, user.getId());
         return ApiResponse.ok("로그인 성공");
+    }
+
+    @LoginCheck
+    @GetMapping("/user/logout")
+    public ApiResponse<String> logout(HttpServletRequest request) {
+        sessionService.removeSession(request);
+        return ApiResponse.ok("로그아웃 성공");
     }
 }
